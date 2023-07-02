db45393109eed2c151eba44f7bde5b419eafb188
Isuru <isurusajith0@gmail.com>
2023-07-02 13:13:26 +0530

    implement post webservice for save customer details, there is auto generated id for the customer, so insertId method used to get generated id for new customer

--- src/api/customer-router.ts
+++ src/api/customer-router.ts
@@ -18,3 +18,28 @@ async function initPool() {
     });
 }
 
+
+type Customer = {
+    id: number, name: string, address: string, contact: string
+};
+
+router.post('/', async (req, res) => {
+    const customer = req.body as Customer;
+    if (!customer.name || !customer.address || !customer.contact) {
+        res.status(400).send("Invalid data");
+    } else {
+        try {
+            const result = await datasource
+                .query('INSERT INTO customer (name, address, contact) VALUES (?, ?, ?)',
+                    [customer.name, customer.address, customer.contact]);
+            customer.id = result.insertId;
+            res.status(201).json(customer);
+        }catch (err: any){
+            if (err.sqlState === '23000'){
+                res.status(409).send("Contact number already exits");
+            }else{
+                throw err;
+            }
+        }
+    }
+});

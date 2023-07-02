735fb2a8353ac0bf0c3983af67da4f82b6239260
Isuru <isurusajith0@gmail.com>
2023-07-02 13:11:09 +0530

    create a database pool, used promise-mysql library

--- src/api/customer-router.ts
+++ src/api/customer-router.ts
@@ -3,3 +3,18 @@ import mysql, {Pool} from 'promise-mysql';
 
 export const router = express.Router();
 
+
+let datasource: Pool;
+
+initPool();
+async function initPool() {
+    datasource = await mysql.createPool({
+        host: 'localhost',
+        port: 3306,
+        database: 'dep10_pos',
+        user: 'root',
+        password: 'mysql',
+        connectionLimit: 10
+    });
+}
+
